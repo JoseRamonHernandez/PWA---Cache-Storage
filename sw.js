0b45d1cca66941c9af81dff805d7499b0ab3fac7
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-16 03:58:49 -0600

    ache with network update

--- sw.js
+++ sw.js
@@ -40,19 +40,17 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
 
-    const respuesta = fetch(e.request).then(res => {
-        if (!res) return caches.match(e.request);
-        console.log('Fetch', res);
-        caches.open(CACHE_DYNAMIC_NAME)
-            .then(cache => {
-                cache.put(e.request, res);
-                limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT);
-            });
+    if (e.request.url.includes('bootstrap')) {
+        return e.respondWith(caches.match(e.request));
+    }
 
-        return res.clone();
-    }).catch(err => {
+    const respuesta = caches.open(CACHE_STATIC_NAME).then(cache => {
+        fetch(e.request).then(newRes =>
+            cache.put(e.request, newRes));
+
+     
         return caches.match(e.request);
-    });
+        });
 
     e.respondWith(respuesta);
 
